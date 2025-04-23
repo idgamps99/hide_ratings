034ceef996e59dc72f9f91ac0b43e8d05367789d
Myles Amphlett <mylesamphlett@gmail.com>
2025-04-23 12:06:31 +0100

    tidy up a bit

--- toggle_ratings.js
+++ toggle_ratings.js
@@ -1,12 +1,29 @@
 const allRatings = document.querySelectorAll(".page_charts_section_charts_item_details_average_num")
+const trackRatings = document.querySelectorAll(".page_release_section_tracks_track_stats_rating pipe_separated")
 const singleRating = document.querySelector(".avg_rating")
 
+function hide (rating) {
+  rating.style.display = "none"
+}
+
+function hideRatings (ratings) {
+  ratings.forEach((rating) => hide(rating))
+}
+
 if (allRatings) {
-  console.log("ALL RATINGS HIDDEN")
-  allRatings.forEach((rating) => rating.style.display = "none")
-} else if (singleRating) {
-  console.log("SINGLE RATING HIDDEN")
-  singleRating.style.display = "none"
+  hideRatings(allRatings)
 } else {
-  console.log("NOTHING SELECTED HERE COWBOY")
+  console.log("no ratings here buddy")
+}
+
+if (trackRatings) {
+  hideRatings(trackRatings)
+} else {
+  console.log("no ratings here buddy")
+}
+
+if (singleRating) {
+  hide(singleRating)
+} else {
+  console.log("no ratings here buddy")
 }
