180029a9f2bfe75797fd77cbf6a60df60490575b
Myles Amphlett <mylesamphlett@gmail.com>
2025-04-23 11:56:36 +0100

    console logs

--- toggle_ratings.js
+++ toggle_ratings.js
@@ -2,9 +2,11 @@ const allRatings = document.querySelectorAll(".page_charts_section_charts_item_d
 const singleRating = document.querySelector(".avg_rating")
 
 if (allRatings) {
+  console.log("ALL RATINGS HIDDEN")
   allRatings.forEach((rating) => rating.style.display = "none")
 } else if (singleRating) {
+  console.log("SINGLE RATING HIDDEN")
   singleRating.style.display = "none"
 } else {
-  console.log("nothing selected here cowboy")
+  console.log("NOTHING SELECTED HERE COWBOY")
 }
